ec0a63d400142e46cc563987fde1f8dd68a37517
Marius Gundersen <me@mariusgundersen.net>
2014-04-06 14:02:44 +0200

    added events to the client test

--- test/testClient.js
+++ test/testClient.js
@@ -5,9 +5,11 @@ var server = http.createServer(function(req, res){
   res.end("hello");
 });
 
-sub.startClient('blabla.localhost', {port: 8281}, server, function(error){
-  console.log("error", error);
-  server.close();
+sub.startClient('blabla.localhost', {port: 8281}, server)
+.on('started', function(port){
+  console.log("app started", port);
+}).on('stopped', function(){
+  console.log('app stopped');
 }).on('error', function(error){
   console.error(error);
 });
